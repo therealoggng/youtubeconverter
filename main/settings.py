76128a8997eb36e182d5c9436eda5cb60b67beaf
Kutman Shirinbekov <61701797+therealoggng@users.noreply.github.com>
2020-04-17 02:47:46 +0600

    Update settings.py
    fix password

--- main/settings.py
+++ main/settings.py
@@ -150,7 +150,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'samyijirnyiflow@gmail.com'
-EMAIL_HOST_PASSWORD = 'hazard97'
+EMAIL_HOST_PASSWORD = '*********'
 
 django_heroku.settings(locals())
 
