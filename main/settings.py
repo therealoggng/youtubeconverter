cd2aca9782230bf6a8b194a1d44f44abc062e523
Zoxon470 <zoxon470@gmail.com>
2020-10-03 17:09:42 +0600

    Refactor main/settings.py

--- main/settings.py
+++ main/settings.py
@@ -1,32 +1,28 @@
 import os
+import dj_database_url
+import environ
 import django_heroku
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+env = environ.Env()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = env.str("SECRET_KEY")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+DEBUG = env.bool("DEBUG", True)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "c++ek5c(fmjfg3*2t)tx+f2km1g#qgjg_)4iw@9gtlgtqrf2-="
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["https://converter-kutman-2.herokuapp.com/"]
-
-
-# Application definition
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS", default=["https://converter-kutman-2.herokuapp.com/"]
+)
 
 INSTALLED_APPS = [
-    "converter",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "converter",
 ]
 
 MIDDLEWARE = [
@@ -59,20 +55,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "main.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -89,10 +77,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -103,36 +87,25 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+BROKER_URL = env.str("CELERY_BROKER_URL", "redis://localhost:6379")
+BROKER_TRANSPORT_OPTIONS = {"visibility_timeout": 3600}
 
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = BROKER_URL
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_SERIALIZER = "json"
 
-
-REDIS_HOST = "localhost"
-REDIS_PORT = "6379"
-BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
-BROKER_TRANSPORT_OPTIONS = {"visibility_timeout": 3600}
-CELERY_RESULT_BACKEND = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
-
-
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "samyijirnyiflow@gmail.com"
-EMAIL_HOST_PASSWORD = "*********"
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
 
 django_heroku.settings(locals())
